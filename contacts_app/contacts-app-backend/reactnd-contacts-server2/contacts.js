3b4baa0d3365849f7a96f2386962f7d7d33ea1c9
sunny0402 <sunnycodes0402@gmail.com>
2022-01-07 17:15:45 -0500

    debug images

--- contacts_app/contacts-app-backend/reactnd-contacts-server2/contacts.js
+++ contacts_app/contacts-app-backend/reactnd-contacts-server2/contacts.js
@@ -9,19 +9,19 @@ const defaultData = {
       id: "richard",
       name: "Richard Kalehoff",
       handle: "@richardkalehoff",
-      avatarURL: config.origin + "/public/richard.jpg",
+      avatarURL: config.origin + "/richard.jpg",
     },
     {
       id: "karen",
       name: "Karen Isgrigg",
       handle: "@karen_isgrigg",
-      avatarURL: config.origin + "/public/karen.jpg",
+      avatarURL: config.origin + "/karen.jpg",
     },
     {
       id: "tyler",
       name: "Tyler McGinnis",
       handle: "@tylermcginnis",
-      avatarURL: config.origin + "/public/tyler.jpg",
+      avatarURL: config.origin + "/tyler.jpg",
     },
   ],
 };
